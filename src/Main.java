4d848c17cc4ad99f59f13fd22d099c4ea973a8e5
Mohammadnaisr <uddinnasir202188@gmail.com>
2024-11-09 07:13:51 +0600

    first commit

--- src/Main.java
+++ src/Main.java
@@ -1,27 +1,17 @@
-
-public class Main {
+public class Main  {
     public static void main(String[] args) {
 
-        int marks = 40;
+        double score = 85.0;
 
-        if (marks >= 80) {
-            System.out.println( "A+"); // A+ 4.00
-        } else if (marks >= 75) {
-            System.out.println("A"); // A 3.75
-        } else if (marks >= 70) {
-            System.out.println(" A-"); // A- 3.50
-        } else if (marks >= 65) {
-            System.out.println("B+"); // B+ 3.25
-        } else if (marks >= 60) {
-            System.out.println("B"); // B 3.00
-        } else if (marks >= 50) {
-            System.out.println("C+"); // C+ 2.50
-        } else if (marks >= 40) {
-            System.out.println("C"); // C 2.00
-        } else if (marks >= 33) {
-            System.out.println("D"); // D 1.00
-        }
+
+        double gpa = (score >= 80 && score <= 100) ? 4.0 :
+                (score >= 70 && score < 80) ? 3.75 :
+                        (score >= 60 && score < 70) ? 3.5 :
+                                (score >= 50 && score < 60) ? 3.25 :
+                                        (score >= 40 && score < 50) ? 3.0 :
+                                                (score >= 33 && score < 40) ? 2.0 : 0.0;
+
+
+        System.out.println("Your GPA is: " + gpa);
     }
 }
-
-
